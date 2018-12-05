b1d8e35b63e33696fd9c66a1edc2d2048b0f4329
Stephen Spalding <sspalding@netflix.com>
2018-12-05 00:17:48 -0800

    No need to fold

--- 2018/05-alchemical-reduction.kts
+++ 2018/05-alchemical-reduction.kts
@@ -10,15 +10,13 @@ import kotlin.math.abs
 
 var polymer = File("input05.txt").readText().trim().toCharArray()
 
-fun react(polymer: CharArray, omitted: Char? = null) = Stack<Char>().apply {
+fun react(polymer: CharArray, omit: Char? = null) = Stack<Char>().apply {
     for (char in polymer)
         if (isNotEmpty() && abs(char - peek()) == 'a' - 'A') pop()  // Found a pair. React!
-        else if (char.toUpperCase() != omitted) push(char)          // Not a pair :(
+        else if (char.toUpperCase() != omit) push(char)          // Not a pair :(
 }.size
 
-val minSize = ('A'..'Z').fold(polymer.size) { minSize, problemPair ->
-    minOf(minSize, react(polymer, problemPair))
-}
-
 println("Part 1 - Reacted size: ${react(polymer)}")
+
+val minSize = ('A'..'Z').map { react(polymer, omit = it) }.min()
 println("Part 2 - Reacted size with problem pair removed: $minSize")
