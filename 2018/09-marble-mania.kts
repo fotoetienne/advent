fffc77d76c96003e53d9b5eaff0f7a76448c4b33
Stephen Spalding <sspalding@netflix.com>
2018-12-08 23:53:29 -0800

    Day 9 part 2

--- 2018/09-marble-mania.kts
+++ 2018/09-marble-mania.kts
@@ -1,5 +1,5 @@
 #!/usr/bin/env kscript
-@file:KotlinOpts("-J-Xmx16g")
+@file:KotlinOpts("-J-Xmx2g")
 @file:KotlinOpts("-J-Xms2g")
 @file:CompilerOpts("-jvm-target 1.8")
 
@@ -12,38 +12,71 @@ import java.io.File
 
 var lines = File("input09.txt").readText()
 
-val nPlayers = 10
-val lastMarble = 1618
-//val nPlayers = 459
-//val lastMarble = 71790
+val nPlayers = 459
+val lastMarble = 7179000L
+//val nPlayers = 10
+//val lastMarble = 1618
+//val nPlayers = 9
+//val lastMarble = 25
 
-val players = (1..nPlayers).map { 0 }.toMutableList()
+val players = (1..nPlayers).map { 0L }.toMutableList()
 
-fun insertMarble(marbles: MutableList<Int>, currentMarbleIndex: Int, nextMarble: Int): Int {
-    val nextMarbleIndex = (currentMarbleIndex + 2) % marbles.size
-    marbles.add(nextMarbleIndex, nextMarble)
-    return nextMarbleIndex
+class Marble<T>(val value: T, previous: Marble<T>? = null, next: Marble<T>? = null) {
+    var previous: Marble<T> = previous ?: this
+    var next: Marble<T> = next ?: this
+
+    fun moveClockwise(n: Int): Marble<T> =
+        if (n == 0) this else next.moveClockwise(n - 1)
+
+    fun moveCounterClockwise(n: Int): Marble<T> =
+        if (n == 0) this else previous.moveCounterClockwise(n - 1)
+
+    fun remove() = next
+        .also {
+            it.previous = previous
+            previous.next = it
+        }
+
+    fun add(value: T) = Marble(value, this, next)
+            .also {
+                next.previous = it
+                next = it
+            }
+
+    fun insertMarble(nextMarble: T) =
+        this.moveClockwise(1).add(nextMarble)
+
+    fun getBonus() =
+        this.moveCounterClockwise(7)
+            .let { Pair(it.value, it.remove()) }
+
+    override fun toString(): String {
+        val list = mutableListOf(value)
+        var nextMarble = next
+        while (nextMarble.value != value) {
+            list.add(nextMarble.value)
+            nextMarble = nextMarble.next
+        }
+        return list.toString()
+    }
 }
 
-fun getBonus(marbles: MutableList<Int>, currentMarbleIndex: Int): Pair<Int, Int> {
-    val bonusMarbleIndex = (currentMarbleIndex - 7 + marbles.size) % marbles.size
-    val bonus = marbles.removeAt(bonusMarbleIndex)
-    return Pair(bonus, bonusMarbleIndex)
-}
-
-val marbles = mutableListOf(0)
-var currentMarbleIndex = 1
+var currentMarble = Marble(0L)
+val marbles = currentMarble
 var player = 1
 
 for (marble in (1..lastMarble)) {
-    if (marble % 23 == 0) {
+//    if (marble % 10_000 == 0L) println(marble)
+//    print("[$marble]")
+    if (marble % 23 == 0L) {
         players[player] += marble
-        val (bonus, bonusIndex) = getBonus(marbles, currentMarbleIndex)
+        val (bonus, nextMarble) = currentMarble.getBonus()
         players[player] += bonus
-        currentMarbleIndex = bonusIndex
+        currentMarble = nextMarble
     } else {
-        currentMarbleIndex = insertMarble(marbles, currentMarbleIndex, marble)
+        currentMarble = currentMarble.insertMarble(marble)
     }
+//    println(marbles)
 
     if (player + 1 == nPlayers) player = 0 else player += 1
 }
