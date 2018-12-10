19de5f1dd29293552aecf7294bc4578ae7928254
Stephen Spalding <sspalding@netflix.com>
2018-12-09 20:55:07 -0800

    Day 9 cleanup

--- 2018/09-marble-mania.kts
+++ 2018/09-marble-mania.kts
@@ -1,25 +1,11 @@
 #!/usr/bin/env kscript
 @file:KotlinOpts("-J-Xmx2g")
 @file:KotlinOpts("-J-Xms2g")
-@file:CompilerOpts("-jvm-target 1.8")
-
-import java.io.File
 
 /**
  * Advent of Code 2018 - Day 9
  * https://adventofcode.com/2018/day/9
  */
-
-var lines = File("input09.txt").readText()
-
-val nPlayers = 459
-val lastMarble = 7179000L
-//val nPlayers = 10
-//val lastMarble = 1618
-//val nPlayers = 9
-//val lastMarble = 25
-
-val players = (1..nPlayers).map { 0L }.toMutableList()
 
 class Marble<T>(val value: T, previous: Marble<T>? = null, next: Marble<T>? = null) {
     var previous: Marble<T> = previous ?: this
@@ -49,37 +35,35 @@ class Marble<T>(val value: T, previous: Marble<T>? = null, next: Marble<T>? = nu
     fun getBonus() =
         this.moveCounterClockwise(7)
             .let { Pair(it.value, it.remove()) }
+}
 
-    override fun toString(): String {
-        val list = mutableListOf(value)
-        var nextMarble = next
-        while (nextMarble.value != value) {
-            list.add(nextMarble.value)
-            nextMarble = nextMarble.next
+fun marbleGame(nPlayers: Int, nMarbles: Int): Long {
+    val players = (1..nPlayers).map { 0L }.toMutableList()
+
+    var currentMarble = Marble(0L)
+    var player = 1
+
+    for (marble in (1..nMarbles)) {
+        if (marble % 23 == 0) {
+            players[player] += marble.toLong()
+            val (bonus, nextMarble) = currentMarble.getBonus()
+            players[player] += bonus
+            currentMarble = nextMarble
+        } else {
+            currentMarble = currentMarble.insertMarble(marble.toLong())
         }
-        return list.toString()
+
+        if (player + 1 == nPlayers) player = 0 else player += 1
     }
+    return players.max()!!
 }
 
-var currentMarble = Marble(0L)
-val marbles = currentMarble
-var player = 1
+//val nPlayers = 9
+//val lastMarble = 25
+//val nPlayers = 10
+//val lastMarble = 1618
+val nPlayers = 459
+val lastMarble = 71790
 
-for (marble in (1..lastMarble)) {
-//    if (marble % 10_000 == 0L) println(marble)
-//    print("[$marble]")
-    if (marble % 23 == 0L) {
-        players[player] += marble
-        val (bonus, nextMarble) = currentMarble.getBonus()
-        players[player] += bonus
-        currentMarble = nextMarble
-    } else {
-        currentMarble = currentMarble.insertMarble(marble)
-    }
-//    println(marbles)
-
-    if (player + 1 == nPlayers) player = 0 else player += 1
-}
-
-println(players.max())
-
+println(marbleGame(nPlayers, lastMarble))
+println(marbleGame(nPlayers, lastMarble * 100))
